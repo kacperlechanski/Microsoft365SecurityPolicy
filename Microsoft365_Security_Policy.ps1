340665b4fe529e3bf1184ea47b2cac97901eab77
kacperlechanski <kacperlechanski19@o2.pl>
2023-12-07 18:52:23 +0100

    Finish AntiPhisingPolicy part

--- Microsoft365_Security_Policy.ps1
+++ Microsoft365_Security_Policy.ps1
@@ -9,33 +9,52 @@ function Login {
 }
 
 #POBRANE ILOŚCI DOMEN I ICH NAZW
-function GetDomains{
-    $domains = @()
-    $x = Read-Host "Ile domen posiadasz w Microsoft 365 ? Policz także domene 'onmicrosoft.com'"
+$domains = @()
+$x = Read-Host "Ile domen posiadasz w Microsoft 365 ? Policz także domene 'onmicrosoft.com'"
 
-    for($i = 1; $i -le $x; $i++){
-        $domain = Read-Host "Podaj nazwę domeny numer $i"
-        $domains += $domain
-    }
-
-    Write-Host "Twoje domeny to: $($domains -join ', ')"
-
-    return $domains
+for($i = 1; $i -le $x; $i++){
+    $domain = Read-Host "Podaj nazwę domeny numer $i"
+    $domains += $domain
 }
+
+Write-Host "Twoje domeny to:"
+
 
 ########POLITYKA ANTYPHISINGOWA########
 function AntiPhisingPolicy {
+$antiphisingPolicyName= Read-Host "Podaj nazwę polityki ANTYPHISINGOWEJ" #podajemy nazwę polityki
+New-AntiPhishPolicy -Name $antiphisingPolicyName
+
 Write-Host "Tworzę politykę ANTYPHISINGOWĄ.." -ForegroundColor Yellow
 Start-Sleep -Seconds 3
 
-$antiphisingPolicyName = Read-Host "Podaj nazwę polityki ANTYPHISINGOWEJ" #podajemy nazwę polityki
-New-AntiPhishPolicy -Name $antiphisingPolicyName
-
 #USTAWIENIA
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -PhishThresholdLevel 2
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableOrganizationDomainsProtection $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableSpoofIntelligence $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableMailboxIntelligence $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableMailboxIntelligenceProtection $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableFirstContactSafetyTips $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableSimilarDomainsSafetyTips $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableSimilarUsersSafetyTips $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableUnusualCharactersSafetyTips $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -EnableViaTag $true
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -HonorDmarcPolicy $true
 
+#AKCJE
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -MailboxIntelligenceProtectionAction MoveToJmf
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -TargetedDomainProtectionAction MoveToJmf
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -TargetedUserProtectionAction MoveToJmf
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -AuthenticationFailAction MoveToJmf
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -DmarcQuarantineAction Quarantine
+Set-AntiPhishPolicy -Identity $antiphisingPolicyName -DmarcRejectAction Reject
+
+#REGUŁA
+#Tutaj powinno być tyle domen ile jest w tenancie (uwzględniająć onmicrosoft.com)
+$phisingRuleName = Read-Host "Podaj nazwę reguły ANTYPHISINGOWEJ" #Podajemy nazwę reguły
+New-AntiphishRule -Name $phisingRuleName -AntiPhishPolicy $antiphisingPolicyName -Enabled $true -Priority 0 -RecipientDomainIs $domains
 }
 
 
-Login
-#GetDomains
-#AntiPhisingPolicy
+#Login
+AntiPhisingPolicy
